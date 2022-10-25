609a7468850b759034b9d1dc9c494798f864c0d2
Elias <eliashelin98@gmail.com>
2022-10-25 17:07:01 +0300

    asdasd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DesktopDatePicker } from '@mui/x-date-pickers';
+import { format } from 'date-fns';
 
 function Todolist() {
-  const [todo, setTodo] = useState({date: '', description: ''});
+  const [todo, setTodo] = useState({ date: new Date(), description: '' });
   const [todos, setTodos] = useState([]);
 
   const inputChanged = (event) => {
-    setTodo({...todo, [event.target.name]: event.target.value});
+    setTodo({ ...todo, [event.target.name]: event.target.value });
+  }
+
+  const dateChanged = (value) => {
+    setTodo({ ...todo, date: value });
   }
 
   const addTodo = (event) => {
@@ -14,25 +25,38 @@ function Todolist() {
 
   const deleteTodo = (index) => {
     const todos2 = todos.filter((todo, i) => i !== index);
-    
+
     setTodos(todos2);
-    }
+  }
 
   return (
-    <div style={{width: '500px', margin: 'auto'}}>
-      <input type="date" onChange={inputChanged} placeholder="Date" name="date" value={todo.date}/>
-      <input type="text" onChange={inputChanged} placeholder="Description" name="description" value={todo.description}/>
-      <button onClick={addTodo}>Add</button>
-    
-      <table>
-        <tr><td style={{width: '110px'}}>Date</td><td>Description</td></tr>
-        <tbody>
-        {
-          todos.map((todo, index) =><tr key={index}><td>{todo.date}</td><td>{todo.description}</td> <button onClick={() => deleteTodo(index)}>Delete</button> </tr>)
-        }
-        </tbody>
-      </table>   
-    </div>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <div style={{ width: '500px', margin: 'auto' }}>
+        <Stack direction="row" spacing={2} justifyContent="center" alignItems="center" marginTop="20px">
+          <DesktopDatePicker
+            label="Date desktop"
+            inputFormat="dd/MM/yyyy"
+            value={todo.date}
+            onChange={dateChanged}
+            renderInput={(params) => <TextField {...params} />}
+          />
+
+          <TextField type="text" onChange={inputChanged} variant="standard"
+            label="Description" name="description" value={todo.description} />
+
+          <Button onClick={addTodo} variant="contained">Add</Button>
+        </Stack>
+        <table style={{ borderSpacing:'8px' }}>
+          <tr><td style={{ width:'230px' }}>Date</td><td>Description</td></tr>
+          <tbody>
+            {
+              todos.map((todo, index) => <tr key={index}><td>{format(todo.date, "dd.MM.yyyy HH.mm")}</td><td>{todo.description}</td> 
+              <button onClick={() => deleteTodo(index)}>Delete</button> </tr>)
+            }
+          </tbody>
+        </table>
+      </div>
+    </LocalizationProvider>
   );
 };
 
